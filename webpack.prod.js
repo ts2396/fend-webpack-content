412d8e465eee64f8f9cbb060e047f07b05e6c9ec
ts2396 <48188551+ts2396@users.noreply.github.com>
2020-04-10 13:23:21 -0400

    commit

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const CleanWebPackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/client/index.js',
@@ -17,6 +18,15 @@ module.exports = {
     new HtmlWebPackPlugin({
       template: './src/client/views/index.html',
       filename: './index.html',
+    }),
+    new CleanWebPackPlugin({
+      // Simulate the removal of files
+      dry: true,
+      // Write Logs to Console
+      verbose: true,
+      // Automatically remove all unused webpack assets on rebuild
+      cleanStaleWebpackAssets: true,
+      protectWebpackAssets: false,
     }),
   ],
 };
